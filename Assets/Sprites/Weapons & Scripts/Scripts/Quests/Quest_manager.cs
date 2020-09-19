5b79e6dcc6d2ae5b916081d2ea543c25a8b76117
quangtien0101 <elwrci.quangtien@gmail.com>
2020-09-19 12:40:07 +0700

    add location_based quest

---
+++ Assets/Sprites/Weapons & Scripts/Scripts/Quests/Quest_manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_manager: MonoBehaviour
+{
+    public Quest_object[] quests;
+    public bool[] quest_completed;
+
+    public Dialogue_manager the_DM;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quest_completed = new bool[quests.Length];     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Show_quest_text(string quest_text)
+    {
+        the_DM.dialog_lines = new string[1];
+        the_DM.dialog_lines[0] = quest_text;
+
+        the_DM.current_line = 0;
+        the_DM.ShowDialogue();
+    }
+}
---
+++ Assets/Sprites/Weapons & Scripts/Scripts/Quests/Quest_object.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_object : MonoBehaviour
+{
+
+    public int quest_number;
+    public Quest_manager the_qm;
+
+    public string start_text;
+    public string end_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Start_quest()
+    {
+        the_qm.Show_quest_text(start_text);
+    }
+
+    public void End_quest()
+    {
+        //display the finish quest diaglouge
+        the_qm.Show_quest_text(end_text);
+
+
+        //mark the quest as completed once we finish it
+        the_qm.quest_completed[quest_number] = true;
+        gameObject.SetActive(false);
+
+    }
+}
