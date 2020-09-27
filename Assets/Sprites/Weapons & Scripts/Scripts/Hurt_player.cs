62a2321f9480e247c283518783dfe0d12283da5e
quangtien0101 <elwrci.quangtien@gmail.com>
2020-09-27 11:54:24 +0700

    Changes to hurt_player script

--- Assets/Sprites/Weapons & Scripts/Scripts/Hurt_player.cs
+++ Assets/Sprites/Weapons & Scripts/Scripts/Hurt_player.cs
@@ -11,12 +11,7 @@ public class Hurt_player : MonoBehaviour
 
     private Player_stats the_player_stats;
 
-    private float attack_time_counter;
-    private bool attacking = false;
-    public float attack_time;
 
-    public float time_between_attack;
-    private float time_between_attack_counter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,47 +22,23 @@ public class Hurt_player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
     }
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.name == "Player")
         {
-            
-            if (time_between_attack_counter <= 0)
-            {
-                attacking = true;
-                attack_time_counter = attack_time;
-            }
 
+            current_damage = damage_to - the_player_stats.current_defense;
+            if (current_damage <= 0)
+                current_damage = 1;
 
-            if (attack_time_counter > 0)
-            {
-                attack_time_counter -= Time.deltaTime;
-                current_damage = damage_to - the_player_stats.current_defense;
-                if (current_damage <= 0)
-                    current_damage = 1;
+            other.gameObject.GetComponent<Player_health>().Hurt_player(current_damage);
+            var clone = (GameObject)Instantiate(damage_number, other.transform.position, Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<Floating_numbers>().damages = current_damage;
 
-                other.gameObject.GetComponent<Player_health>().Hurt_player(current_damage);
-                var clone = (GameObject)Instantiate(damage_number, other.transform.position, Quaternion.Euler(Vector3.zero));
-                clone.GetComponent<Floating_numbers>().damages = current_damage;
-
-            }
-
-            if (time_between_attack_counter > 0)
-            {
-                time_between_attack_counter -= Time.deltaTime;
-            }
-
-            if (attack_time_counter <= 0)
-            {
-                attacking = false;
-                time_between_attack_counter = time_between_attack;
-            }
-            
         }
-       
-      
+
     }
-    
+
 }
